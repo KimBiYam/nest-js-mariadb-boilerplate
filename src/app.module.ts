47e98cb5e955aa29b78b240176d08c3d8d02922c
ChangHyunKim <hot9998@naver.com>
2021-02-14 15:52:14 +0900

    Removed auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { ormconfig } from './ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), AuthModule],
+  imports: [TypeOrmModule.forRoot(ormconfig)],
   controllers: [AppController],
   providers: [AppService],
 })
