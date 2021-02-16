fc063397b958e740750792c2283e9fcfd0a2a15d
ChangHyunKim <hot9998@naver.com>
2021-02-16 14:00:54 +0900

    Fix run script shell file
    doesn't exist node_modules directory then npm install

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
+if [ ! -d node_modules ]; then
+    npm install
+    echo "done"
+fi
+
 docker-compose -f docker/docker-compose.yml up --build
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import LoginAuthDto from './dto/login-auth.dto';
