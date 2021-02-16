7395607c8045f060042bd607305d4ff0f61118be
ChangHyunKim <hot9998@naver.com>
2021-02-16 22:38:36 +0900

    Add CreateUserDto's swagger example values

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -2,19 +2,19 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty()
+  @ApiProperty({ example: 'userId' })
   @IsString()
   userId: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: 'name' })
   @IsString()
   name: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: 'userId@gmail.com' })
   @IsEmail()
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: '12345678' })
   @IsString()
   @MinLength(8)
   @MaxLength(16)
