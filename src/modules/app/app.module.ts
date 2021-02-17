de27ab8160eedc3f48b11aa1a8030c91f07868c1
ChangHyunKim <hot9998@naver.com>
2021-02-17 22:20:28 +0900

    Remove the app module's config module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,12 +7,7 @@ import { AuthModule } from '../auth/auth.module';
 import { UserModule } from '../user';
 
 @Module({
-  imports: [
-    ConfigModule,
-    TypeOrmModule.forRoot(ormconfig),
-    UserModule,
-    AuthModule,
-  ],
+  imports: [TypeOrmModule.forRoot(ormconfig), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
