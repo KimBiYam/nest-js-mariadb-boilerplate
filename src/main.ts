71f19332d7857f1a16d3b696011b8f0da843d810
ChangHyunKim <hot9998@naver.com>
2021-03-02 22:57:53 +0900

    Setup swagger only development mode

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { setupSwagger } from './config';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  setupSwagger(app);
+  const configService = app.get(ConfigService);
+
+  if (configService.get('NODE_ENV') === 'DEVELOPMENT') {
+    setupSwagger(app);
+  }
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
