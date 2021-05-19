ece4d3fe5317ad80f06e59934ce50186a067bb8d
ChangHyunKim <hot9998@naver.com>
2021-05-19 14:23:18 +0900

    Fix post controller unit test's structure

--- src/modules/post/post.controller.spec.ts
+++ src/modules/post/post.controller.spec.ts
@@ -1,30 +1,10 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { User } from 'src/decorators';
 import { PostController, PostService } from '.';
 import { UserEntity, UserService } from '../user';
 import { CreatePostDto } from './dto/create-post.dto';
 import { PostEntity } from './post.entity';
-
-const mockUserService = {
-  findOneByUserId: jest.fn((id) => {
-    return { ...testUser, id };
-  }),
-};
-
-const mockPostService = {
-  findAll: jest.fn(() => [mockPostEntity]),
-  findOneByPostId: jest.fn((id: number) => {
-    return { ...mockPostEntity, id };
-  }),
-  create: jest.fn(() => {
-    return mockPostEntity;
-  }),
-  delete: jest.fn(() => {}),
-  update: jest.fn(({ content, title }: CreatePostDto, id: number) => {
-    return { ...mockPostEntity, id, content, title };
-  }),
-  validatePostUser: jest.fn(() => true),
-};
 
 const testUser: UserEntity = {
   id: 1,
@@ -36,7 +16,7 @@ const testUser: UserEntity = {
   isActive: true,
 };
 
-const mockPostEntity: PostEntity = {
+const testPostEntity: PostEntity = {
   content: 'content',
   id: 1,
   title: 'title',
@@ -46,7 +26,9 @@ const mockPostEntity: PostEntity = {
 };
 
 describe('PostController', () => {
-  let controller: PostController;
+  let postController: PostController;
+  let userSerivce: UserService;
+  let postService: PostService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -54,40 +36,74 @@ describe('PostController', () => {
       providers: [
         {
           provide: UserService,
-          useValue: mockUserService,
+          useValue: {
+            findOneByUserId: jest.fn((id) => {
+              return { ...testUser, id };
+            }),
+          },
         },
         {
           provide: PostService,
-          useValue: mockPostService,
+          useValue: {
+            findAll: jest.fn(() => [testPostEntity]),
+            findOneByPostId: jest.fn((id: number) => {
+              return { ...testPostEntity, id };
+            }),
+            create: jest.fn(() => {
+              return testPostEntity;
+            }),
+            delete: jest.fn(() => {}),
+            update: jest.fn(({ content, title }: CreatePostDto, id: number) => {
+              return { ...testPostEntity, id, content, title };
+            }),
+            validatePostUser: jest.fn(() => true),
+          },
         },
       ],
     }).compile();
 
-    controller = module.get<PostController>(PostController);
+    postController = module.get<PostController>(PostController);
+    userSerivce = module.get<UserService>(UserService);
+    postService = module.get<PostService>(PostService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(postController).toBeDefined();
   });
 
   it('should create a post', async () => {
-    const response = (await controller.createPost(
-      { userId: '1', username: 'userName' },
-      { content: 'content', title: 'title' },
+    const user: User = { userId: '1', username: 'userName' };
+    const createPostDto: CreatePostDto = { content: 'content', title: 'title' };
+
+    const response = (await postController.createPost(
+      user,
+      createPostDto,
     )) as any;
 
     expect(response.id).toEqual(1);
   });
 
+  it('should throw NotFoundException', async () => {
+    const user: User = { userId: '1', username: 'userName' };
+    const createPostDto: CreatePostDto = { content: 'content', title: 'title' };
+
+    try {
+      userSerivce.findOneByUserId = jest.fn((id) => undefined);
+      await postController.createPost(user, createPostDto);
+    } catch (e) {
+      expect(e).toBeInstanceOf(NotFoundException);
+    }
+  });
+
   it('should get posts', async () => {
-    const response = await controller.getPosts();
+    const response = await postController.getPosts();
 
     expect(response.length).toBeGreaterThan(0);
   });
 
   it('should get a post', async () => {
     const postId = 1;
-    const response = await controller.getPost(postId);
+    const response = await postController.getPost(postId);
 
     expect(response.id).toEqual(postId);
   });
@@ -101,8 +117,8 @@ describe('PostController', () => {
     };
     const postId = 1;
 
-    await controller.createPost(user, createPostDto);
-    const response = (await controller.updatePost(
+    await postController.createPost(user, createPostDto);
+    const response = (await postController.updatePost(
       user,
       postId,
       updatePostDto,
@@ -116,11 +132,11 @@ describe('PostController', () => {
     const createPostDto: CreatePostDto = { content: 'create', title: 'create' };
     const postId = 1;
 
-    await controller.createPost(user, createPostDto);
-    await controller.deletePost(user, postId);
-    mockPostService.findAll = jest.fn(() => []);
+    await postController.createPost(user, createPostDto);
+    await postController.deletePost(user, postId);
+    postService.findAll = jest.fn(async () => []);
 
-    const response = await controller.getPosts();
+    const response = await postController.getPosts();
     expect(response.length).toEqual(0);
   });
 });
